45253475fc822a23eef61e2e21d11788cb2e2ae7
Fidele K <itfidele@gmail.com>
2022-10-07 10:59:09 +0300

    add pages

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -3,9 +3,8 @@ import { LinkMenuType } from "../types/menus";
 
 class LinkButton extends React.Component<LinkMenuType>{
     render(){
-        return <div className="hidden hover:scale-[1.5]  hover:rounded-md hover:px-2 hover:bg-blue-400 hover:text-white transition-all group-hover:inline-flex">{this.props.text}</div>
+        return <div className="hidden hover:scale-[1.2]  hover:rounded-md hover:px-2 transition-all group-hover:inline-flex">{this.props.text}</div>
     }
 }
-
 
 export default LinkButton
