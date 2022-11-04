69edef595bb77572d3cc70dc916efca24db98191
Fidele K <itfidele@gmail.com>
2022-11-04 12:38:55 +0300

    updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -105,11 +105,11 @@ const Home:React.FC=()=>{
                     </div>
                 })}
                 
-                <div className='w-full h-48 p-2 text-3xl justify-center items-center flex font-thin box-border bg-gray-400'>
+                <a href="https://github.com/itfidele" className='w-full h-48 p-2 text-3xl justify-center items-center flex font-thin box-border bg-gray-400'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 text-gray-800 h-16">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
                     </svg>
-                </div>
+                </a>
             </div>
         
         </Section>
